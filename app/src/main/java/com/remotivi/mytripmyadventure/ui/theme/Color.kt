66ab0b108d191d33a7dfe71acf40349a004596f8
satriyoskibidi <poetrysatriyo@gmail.com>
2026-04-23 12:00:12 +0700

    APA YA

---
+++ app/src/main/java/com/remotivi/mytripmyadventure/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.remotivi.mytripmyadventure.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkGreen = Color(0xFF32473D)
+val LightCream = Color(0xFFF2EAD3)
+val LightGrey = Color(0xFFEFEFEF)
+val PriceOrange = Color(0xFFE67E22)
+val TextGrey = Color(0xFF7F8C8D)
